917127126bd9ae255834ed082f86e9985ecf0fd0
Zongkai <59053963+ZongkaiLiu@users.noreply.github.com>
2020-02-22 19:07:34 +0800

    画图
    matplotlib.pyplot 画sin

--- demo.py
+++ demo.py
@@ -1,2 +1,11 @@
 import matplotlib.pyplot as plt
-import torch
+import numpy as np
+
+x = np.linspace(0, 2*np.pi, 51)
+y = np.sin(x)
+
+plt.figure()
+plt.plot(x, y, 'ro-')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
